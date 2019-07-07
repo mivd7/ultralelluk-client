6dc74edca6ea7cb05eade0483aeb52cf86822e5f
MIVD <mi.vandeurzen@gmail.com>
2019-07-07 15:35:34 +0200

    remove apikey

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,3 @@
 export const AUTH_TOKEN = 'auth-token'
 
 export const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${process.env.REACT_APP_CALENDAR_ID}/events?key=${process.env.REACT_APP_CALENDAR_KEY}`
-
-//`https://www.googleapis.com/calendar/v3/calendars/49fip3qh20d30jt7er6n1o30vo@group.calendar.google.com/events?key=AIzaSyBg2f1StYrGjdtUQVYBdvjEyrn78xOJAvQ`
-
-// export function getEvents (callback) {
-//   request
-//     .get(url)
-//     .end((err, resp) => {
-//       if (!err) {
-//         const events = []
-//         JSON.parse(resp.text).items.map((event) => {
-//           events.push({
-//             start: event.start.date || event.start.dateTime,
-//             end: event.end.date || event.end.dateTime,
-//             title: event.summary,
-//           })
-//         })
-//         callback(events)
-//       }
-//     })
-// }
