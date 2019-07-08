5f9c9972f980d8dd35f4e3d48d297beb609e8ade
MIVD <mi.vandeurzen@gmail.com>
2019-07-08 21:16:01 +0200

    edit icon

--- src/components/CustomCalendar.js
+++ src/components/CustomCalendar.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import { Calendar } from 'react-big-calendar'
 import CalendarEventDialog from './CalendarEventDialog';
-import Button from '@material-ui/core/Button';
-// import TextField from '@material-ui/core/TextField';
-// import Dialog from '@material-ui/core/Dialog';
-// import DialogActions from '@material-ui/core/DialogActions';
-// import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
-// import DialogTitle from '@material-ui/core/DialogTitle';
 
 const CustomCalendar = ({ localizer, events, views }) => {
-  const [open, setOpen] = React.useState(true)
-  //to do event details popup window + edit & delete function!
-
-  function handleClickOpen() {
-    setOpen(true);
-  }
-
-  function handleClose() {
-    setOpen(false);
-  }
-
   function EventDialog({ event }) {
     return (
       <div>
-        {event.title}       
+        {event.title}
         <CalendarEventDialog eventId={event.id}/>
     </div>
     )
@@ -40,7 +22,6 @@ const CustomCalendar = ({ localizer, events, views }) => {
           step={60}
           showMultiDayTimes
           defaultDate={new Date()}
-          onSelectEvent={() => handleClickOpen()}
           components={
             {event: EventDialog}
           }
